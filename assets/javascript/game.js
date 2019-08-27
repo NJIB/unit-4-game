cb54ee17a2baa0a7aa69b57508b74e9f831197bc
Nicholas Buck <njibuck@gmail.com>
2019-08-26 23:24:47 -0400

    Loyout tweaked to include rocket track etc.  Random number generator working for tgtNumber and crystalDiv

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,58 @@
-var tgtNumber = [];  // tgtNumber is the number the computer randomly selects, between 19 and 120
+var tgtNumber = 0;  // tgtNumber is the number the computer randomly selects, between 19 and 120
 var latestTotal = [];  // Holds the total of all numbers guessed so far
+// var crystalNumbers = {
+//     crystal1: 0,
+//     crystal2: 0,
+//     crystal3: 0,
+//     crystal4: 0
+// };
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+var crysNumLog = [];
+
 
 
 // Computer randomly chooses a team
-function chooseNumber() {
-    while (tgtNumber < 20){
+function chooseTgtNumber() {
+    // while (Number < 20) {
         tgtNumber = Math.floor(Math.random() * 121);
-    }
-    console.log(tgtNumber);
+    // }
+    console.log("tgtNumber:" + tgtNumber);
     document.getElementById("targetNumber").innerHTML = tgtNumber;
 }
 
-// function checkWinLose() {
-//     if ((totalGuessed >= teamNameLength) && (guessesRemaining > 0)) {
-//         selectTeamBadge(chosenTeam);
-//         // document.getElementById("teamBadge").style.display = "flex";
-//         document.getElementById("pressToRestart").style.display = "flex";
-//         endResult = ("Goal...! &nbsp You got it - the correct answer was &nbsp" + chosenTeam);
-//         document.getElementById("letterContainer").innerHTML = endResult;
-//         gamesWon++;
-//         document.getElementById("totalScore").innerHTML = "TOTAL SCORE :  " + gamesWon;
-//         console.log(gamesWon);
-//     } else
+function chooseCrystalNumber(crystal, crystalDiv) {
+    // while (crystal1 < 1) {
+    //     crystal1 = Math.floor(Math.random() * 11);
+    // }
+    //     console.log("crystal1:" + crystal1);
+    //     crysNumLog.push(crystal1);
+    //     console.log("crysNumLog (#1 pushed):" + crysNumLog)
+    //     //ASSIGN THIS NUMBER TO CRYSTAL 1
+    //     document.getElementById("Crystal1").innerHTML = crystal1;
 
-//         if ((totalGuessed < teamNameLength) && (guessesRemaining < 1)) {
-//             selectTeamBadge(chosenTeam);
-//             document.getElementById("teamBadge").style.display = "flex";
-//             document.getElementById("pressToRestart").style.display = "flex";
-//             endResult = ("Foul...! &nbsp The correct answer was&nbsp" + chosenTeam);
-//             document.getElementById("letterContainer").innerHTML = endResult;
-//         }
-// }
+        while (crystal < 1  || crysNumLog.indexOf(crystal) > -1) {
+            crystal = Math.floor(Math.random() * 11);
+        }
+            crysNumLog.push(crystal);
+            console.log("crysNumLog:" + crysNumLog)
+            //ASSIGN THIS NUMBER TO CRYSTAL 1
+            document.getElementById(crystalDiv).innerHTML = crystal;
+    
+}
+
+
+
+function checkWinLose() {
+    if (latestTotal < tgtNumber) {
+    } else if (latestTotal === tgtNumber) {
+            document.getElementById("rocketTrack").innerHTML = "You win!";
+        } else if (latestTotal > tgtNumber) {
+            document.getElementById("rocketTrack").innerHTML = "You overshot!";
+        }
+}
 
 // function displayUpdate(playerScoreCount, computerScoreCount) {
 //     document.getElementById("playerScore").innerHTML = playerScoreCount;
@@ -49,8 +71,13 @@ function chooseNumber() {
 
 
 //FUNCTION CALLS FOR PROGRAM EXECUTION   
-playGame()
+// playGame()
 
-function playGame() {
-    chooseNumber();
-}
+// function playGame() {
+    chooseTgtNumber();
+    chooseCrystalNumber(crystal1, "Crystal1");
+    chooseCrystalNumber(crystal2, "Crystal2");
+    chooseCrystalNumber(crystal3, "Crystal3");
+    chooseCrystalNumber(crystal4, "Crystal4");
+    
+    // }
